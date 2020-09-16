4a8a19d52eaebb5cca38f241d5e78a9535e84175
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-16 21:31:24 +0700

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -85,6 +85,95 @@ EMOJI_PATTERN = re.compile(
     "\U00002702-\U000027B0"  # Dingbats 
     "]+")
 
+def is_it_animated_sticker(message):
+    try:
+        if message.media and message.media.document:
+            mime_type = message.media.document.mime_type
+            if "tgsticker" in mime_type:
+                return True
+            else:
+                return False
+        else:
+            return False
+    except:
+        return False
+
+
+def is_message_image(message):
+    if message.media:
+        if isinstance(message.media, MessageMediaPhoto):
+            return True
+        if message.media.document:
+            if message.media.document.mime_type.split("/")[0] == "image":
+                return True
+        return False
+    return False
+
+
+async def silently_send_message(conv, text):
+    await conv.send_message(text)
+    response = await conv.get_response()
+    await conv.mark_read(message=response)
+    return response
+
+
+async def stickerset_exists(conv, setname):
+    try:
+        await borg(GetStickerSetRequest(InputStickerSetShortName(setname)))
+        response = await silently_send_message(conv, "/addsticker")
+        if response.text == "Invalid pack selected.":
+            await silently_send_message(conv, "/cancel")
+            return False
+        await silently_send_message(conv, "/cancel")
+        return True
+    except StickersetInvalidError:
+        return False
+
+
+def resize_image(image, save_locaton):
+    """ Copyright Rhyse Simpson:
+        https://github.com/skittles9823/SkittBot/blob/master/tg_bot/modules/stickers.py
+    """
+    im = Image.open(image)
+    maxsize = (512, 512)
+    if (im.width and im.height) < 512:
+        size1 = im.width
+        size2 = im.height
+        if im.width > im.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        im = im.resize(sizenew)
+    else:
+        im.thumbnail(maxsize)
+    im.save(save_locaton, "PNG")
+
+
+def progress(current, total):
+    logger.info("Uploaded: {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
+
+
+def find_instance(items, class_or_tuple):
+    for item in items:
+        if isinstance(item, class_or_tuple):
+            return item
+    return None
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+            os.remove(os.path.join(root, file))
+
 @borg.on(admin_cmd(pattern=r"qbot(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
