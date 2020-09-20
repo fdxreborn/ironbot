dd7e33bb04891c333156cb86aad324949e2931e8
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-20 14:57:45 +0700

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -22,36 +22,41 @@ from googletrans import Translator
 from userbot.utils import admin_cmd
 
  
-@borg.on(admin_cmd("sm ?(.*)"))
+@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
+        return
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
+        await event.edit("`Reply to any user message.`")
+        return
+    reply_message = await event.get_reply_message()
     if not reply_message.text:
-       await event.edit("```reply to text message```")
-       return
+        await event.edit("```reply to text message```")
+        return
     chat = "@SangMataInfo_bot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
-    await event.edit("```Processing```")
-    async with borg.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1178524273))
-             # await borg.forward_messages(chat, reply_message)
-              await silently_send_message(chat,"/generate")
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
-              return
-          if response.text.startswith("Forward"):
-             await event.edit("```can you kindly disable your forward privacy settings for good?```")
-          else: 
-             await event.edit(f"{response.message.message}")     
+    await event.edit("`Processing`")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=461843263)
+                )
+                await bot.forward_messages(chat, reply_message)
+                response = await response
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @sangmatainfo_bot and try again`")
+                return
+            if response.text.startswith("Forward"):
+                await event.edit(
+                    "`Can you kindly disable your forward privacy settings for good?`"
+                )
+            else:
+                await event.edit(f"{response.message.message}")
+                await bot.send_read_acknowledge(conv.chat_id)
+    except TimeoutError:
+        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
+
     
 @borg.on(admin_cmd("ud (.*)"))
 async def _(event):
