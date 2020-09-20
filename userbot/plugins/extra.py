e505d86d52f208949e6a7c6f820b88294fc89803
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-20 16:08:54 +0700

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -22,52 +22,36 @@ from googletrans import Translator
 from userbot.utils import admin_cmd
 
  
-@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
-async def lastname(steal):
-    if steal.fwd_from:
-        return
-    if not steal.reply_to_msg_id:
-        await steal.edit("`Reply to any user message.`")
-        return
-    message = await steal.get_reply_message()
+@borg.on(admin_cmd("sg ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```reply to text message```")
+       return
     chat = "@SangMataInfo_bot"
-    user_id = message.sender.id
-    id = f"/search_id {user_id}"
-    if message.sender.bot:
-        await steal.edit("`Reply to actual users message.`")
-        return
-    await steal.edit("`Hmmm.....`")
-    try:
-        async with bot.conversation(chat) as conv:
-            try:
-                msg = await conv.send_message(id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await steal.reply("`Please unblock @sangmatainfo_bot and try again`")
-                return
-            if response.text.startswith("No records found"):
-                await steal.edit("`No records found for this user`")
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id]
-                )
-                return
-            else:
-                respond = await conv.get_response()
-                await steal.edit(f"`{response.message}`")
-            await steal.client.delete_messages(
-                conv.chat_id, [msg.id, r.id, response.id, respond.id]
-            )
-    except TimeoutError:
-        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
-
-
-CMD_HELP.update(
-    {
-        "sangmata": ">`.sg` \
-          \nUsage: View user history.\n"
-    }
-)
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```Processing```")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+             # await borg.forward_messages(chat, reply_message)
+              await silently_send_message(chat,"/generate")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.edit(f"{response.message.message}")
 
     
 @borg.on(admin_cmd("ud (.*)"))
