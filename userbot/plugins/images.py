d3363933ce1cf2c37178011422634d3ffa56cb73
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 17:33:13 +0700

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -27,7 +27,7 @@ async def img_sampler(event):
         return await edit_or_reply(
             event, "Reply to a message or pass a query to search!"
         )
-    await event.edit("`Processing...`")
+    await event.edit("`Mencari gambar..`")
     if event.pattern_match.group(1) != "":
         lim = int(event.pattern_match.group(1))
         if lim > 10:
