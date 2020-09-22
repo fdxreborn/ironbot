9dd8f77ff458b74b28009a45ff88e3890352ecae
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 17:23:37 +0700

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -13,7 +13,7 @@ from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @borg.on(admin_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)"))
-@borg.on(sudo_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)", allow_sudo=True))
+#@borg.on(sudo_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)", allow_sudo=True))
 async def img_sampler(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
