0de942a9c1913922ca68c0e22211e0262ad70990
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 13:10:34 +0700

    Update IronUpdater.py

--- userbot/plugins/IronUpdater.py
+++ userbot/plugins/IronUpdater.py
@@ -202,8 +202,8 @@ async def upstream(event):
 
     if changelog == "" and not force_update:
         await event.edit(
-            "\n`➰ Ironbot Info ➰`\n\n`Versi   :`  **terbaru** 🟢\n`Base on :`  "
-            f"**{UPSTREAM_REPO_BRANCH}** 🔶\n"
+            "\n`➰ Ironbot Info ➰`\n\n🔺 `Versi   :`  **terbaru** 🔺\n🔺 `Base on :`  "
+            f"**{UPSTREAM_REPO_BRANCH}** 🔺\n"
         )
         await asyncio.sleep(3)
         await event.delete()
