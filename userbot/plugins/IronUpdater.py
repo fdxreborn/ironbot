b21b0f01045219940f124f68072dbdb45a8e5298
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 12:52:17 +0700

    Update IronUpdater.py

--- userbot/plugins/IronUpdater.py
+++ userbot/plugins/IronUpdater.py
@@ -92,7 +92,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         await event.edit(
-            "`[HEROKU]`" "\n`Ironbot dyno build masih proses, tunggu ya...`"
+            "`[IRONBOT]`" "\n⏳` Deploy proses, tunggu ya... `⏳"
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
