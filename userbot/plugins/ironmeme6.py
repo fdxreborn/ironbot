f907f02c7242c94e7fd9ddaa656a40b983029631
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 15:10:15 +0700

    Update ironmeme6.py

--- userbot/plugins/ironmeme6.py
+++ userbot/plugins/ironmeme6.py
@@ -252,7 +252,7 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 1
+    animation_interval = .1
     animation_ttl = range(0, 14)
  #   input_str = event.pattern_match.group(1)
  #   if input_str == 'fnl':
