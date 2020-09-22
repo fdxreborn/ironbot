6a628c4465fe61840a6092f9cf84922e09c49e32
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 12:33:17 +0700

    Update IronUpdater.py

--- userbot/plugins/IronUpdater.py
+++ userbot/plugins/IronUpdater.py
@@ -92,7 +92,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         await event.edit(
-           "" "`⚡Deploying ironbot, check .alive jika sudah 5-10min...`"
+            "" "`⚡Deploying ironbot, check .alive jika sudah 5-10min...`"
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
