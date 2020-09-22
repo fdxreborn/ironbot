fdc56fc87362e7e5d2cdddf7895f04e67b4b0165
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-22 13:41:35 +0700

    Update IronUpdater.py

--- userbot/plugins/IronUpdater.py
+++ userbot/plugins/IronUpdater.py
@@ -225,7 +225,7 @@ async def upstream(event):
     if conf == "" and not force_update:
       #  await print_changelogs(event, ac_br, changelog)
        # await event.delete()
-        await event.edit("** A D A  U P D A T E  G A N **\n\n`Ketik : •.update now/.update deploy untuk update ironbot.`")
+        await event.edit(f"** A D A  U P D A T E  G A N **\n{ac_br}\n`Ketik : •.update now/.update deploy untuk update ironbot.`")
        # await event.respond("`Ketik : •.update now/.update deploy untuk update ironbot.`")
 
     if force_update:
