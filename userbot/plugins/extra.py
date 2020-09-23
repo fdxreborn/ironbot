327973258b8d55a72ec43be2e8a5bb63d16e9d7e
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-23 17:24:21 +0700

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -311,6 +311,36 @@ async def endecrypt(query):
                                validate=True))[2:]
         await query.reply("Decoded: `" + lething[:-1] + "`")
 
+@register(outgoing=True, pattern=r"^\.smd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+        await asyncio.sleep(2)
+        await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=752979930)
+            )
+            msg = await bot.send_message(chat, link)
+            respond = await response
+            res = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=752979930)
+            )
+            r = await res
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.reply(
+                "```Please unblock @SpotifyMusicDownloaderBot and try again```"
+            )
+            return
+        await bot.forward_messages(event.chat_id, respond.message)
+    await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
+    await event.delete()
+
 
 CMD_HELP.update({"base64": "Find the base64 encoding of the given string"})
 
