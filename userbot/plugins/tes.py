bc1bb250bd1adb427044dceca8985bc4f99c7e7d
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 12:49:46 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -1,3 +1,11 @@
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+
 @register(outgoing=True, pattern="^.nhentai(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
