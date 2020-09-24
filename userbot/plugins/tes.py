56aaf0b9a79efd98b036d3d0f6ae0f143e78fd2a
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 12:06:05 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -1,43 +1,51 @@
-from telethon import events, functions
-import asyncio, io, types
-from uniborg.util import admin_cmd
-from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from uniborg.util import admin_cmd
-from userbot import CMD_HELP, bot
-from subprocess import PIPE
-from subprocess import run as runapp
-from userbot.events import register, errors_handler
 from asyncio.exceptions import TimeoutError
 
-@register(outgoing=True, pattern=r"^\.hc (?:(now)|(.*) - (.*))")
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import CMD_HELP, bot
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.hc(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
+    if not event.reply_to_msg_id:
+        await event.edit("`Reply to any user message.`")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.text:
+        await event.edit("```reply to text message```")
+        return
     chat = "@hcdecryptor_bot"
+    await event.edit("`Processing`")
     try:
-        await event.edit("`Getting Your Music`")
         async with bot.conversation(chat) as conv:
-            await asyncio.sleep(2)
-            await event.edit("`Downloading...`")
             try:
                 response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=1025667425)
+                    events.NewMessage(incoming=True, from_users=461843263)
                 )
-                msg = await bot.send_message(chat)
-                respond = await response
-                res = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=1025667425)
-                )
-                r = await res
+                await bot.forward_messages(chat, reply_message)
+                response = await response
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
-                await event.reply("`Unblock `@SpotifyMusicDownloaderBot` and retry`")
+                await event.reply("`Please unblock @sangmatainfo_bot and try again`")
                 return
-            await bot.forward_messages(event.chat_id, respond.message)
-        await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
-        await event.delete()
+            if response.text.startswith("Forward"):
+                await event.edit(
+                    "`Can you kindly disable your forward privacy settings for good?`"
+                )
+            else:
+                await event.edit(f"{response.message.message}")
+                await bot.send_read_acknowledge(conv.chat_id)
     except TimeoutError:
-        return await event.edit(
-            "`Error: `@SpotifyMusicDownloaderBot` is not responding or Song not found!.`"
-        )
+        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
+
+
+CMD_HELP.update(
+    {
+        "sangmata": ">`.hc` \
+          \nUsage: View user history.\n"
+    }
+)
