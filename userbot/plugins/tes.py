41cfe5f6325b5c937081a7c7cda3c863a1fa6a22
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 20:21:18 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -37,3 +37,29 @@ async def _(event):
             )
         else:
             await event.edit(f"`{response.message.message}`")
+
+@borg.on(admin_cmd(pattern="snif ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    reply_message = await event.get_reply_message()
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    chat = "@AsalAja777_bot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await event.client.send_message(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("Unblock @vixtbot")
+              return
+          if response.text.startswith("I can't find that"):
+             await event.edit("sorry i can't find it")
+          else: 
+             await event.delete()
+             await borg.send_file(event.chat_id, response.message, reply_to=reply_to_id)
+
+
