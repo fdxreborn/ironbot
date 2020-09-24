e30794d52a771a1bdecb58cff8f4f80591ee2912
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 19:37:00 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -39,3 +39,29 @@ async def _(event):
              await bot.forward_messages(event.chat_id, response.message)
              await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
              await event.delete()
+
+@register(outgoing=True, pattern="^\.sniff(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@AsalAja777_bot"
+    await event.edit("```Sedang mengambil musik yang anda inginkan```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await event.edit("`Sedang Di Download, Stay disini aja`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              msg = await bot.send_message(chat, link)
+              respond = await response
+              res = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              r = await res
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await event.reply("```Tolong DiUnblock @SpotifyMusicDownloaderBot dan coba lagi```")
+              return
+          await bot.forward_messages(event.chat_id, respond.message)
+    await event.client.delete_messages(conv.chat_id,
+                                       [msg.id, r.id, respond.id])
+    await event.delete()
