eff98d67a0f74eb86dad5adbcd2d5274c5dc0f9f
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 20:26:49 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -1,65 +1,45 @@
-from asyncio.exceptions import TimeoutError
-import datetime
-import os
-import shutil
-import time
-import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot.events import register
 from userbot import bot, CMD_HELP
-from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern=("hc ?(.*)")))
+
+@register(outgoing=True, pattern="^.hc(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("```Reply to any user message.```")
+        await event.edit("`Reply to any user message.`")
         return
     reply_message = await event.get_reply_message()
+    # if not reply_message.text:
+    # await event.edit("```reply to text message```")
+    # return
     chat = "@AsalAja777_bot"
-    message_id_to_reply = event.message.reply_to_msg_id
-    await event.edit("```Hemmm.....```")
-    async with borg.conversation(chat) as conv:
+    reply_message.sender
+    if reply_message.sender.bot:
+        await event.edit("`Reply to actual users message.`")
+        return
+    await event.edit("`Hmm...`")
+    async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=461843263)
-            )
-            await borg.forward_messages(chat, reply_message)
+                events.NewMessage(
+                    incoming=True,
+                    from_users=461843263))
+            await bot.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+            await event.reply("`Please unblock   and try again`")
             return
         if response.text.startswith("Forward"):
-            await event.edit(
-                "The user have enabled privacy settings you cant get name history"
-            )
+            await event.edit("`can you kindly disable your forward privacy settings for good?`")
         else:
-            await event.edit(f"`{response.message.message}`")
-
-@borg.on(admin_cmd(pattern="snif ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return 
-    reply_message = await event.get_reply_message()
-    reply_to_id = event.message
-    if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    chat = "@AsalAja777_bot"
-    await event.edit("```Checking...```")
-    async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
-              await event.client.send_message(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("Unblock @vixtbot")
-              return
-          if response.text.startswith("I can't find that"):
-             await event.edit("sorry i can't find it")
-          else: 
-             await event.delete()
-             await borg.send_file(event.chat_id, response.message, reply_to=reply_to_id)
+            await event.edit(f"{response.message.message}")
 
 
+CMD_HELP.update({
+    "snif":
+        ".snif \
+          \nreply to files for view proxy or sni .\n"
+})
