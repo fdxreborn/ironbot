e6df250bf1e1fec4ea8bf33bed6e4671a57c17db
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 22:25:37 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -43,3 +43,38 @@ async def _(event):
         else:
             await event.edit(f"```{response.message.message}```")
 
+@register(outgoing=True, pattern="^.sniff(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.edit("```Reply to any user message.```")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.text:
+        await event.edit("```Reply to text message```")
+        return
+    chat = "@AsalAja777_bot"
+    reply_message.sender
+    if reply_message.sender.bot:
+        await event.edit("```Reply to actual users message.```")
+        return
+    await event.edit("```Making a Quote```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=835283956)
+            )
+            await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
+            return
+        await borg.send_read_acknowledge(conv.chat_id)
+        if response.text.startswith("Hi!"):
+            await event.edit(
+                "```Can you kindly disable your forward privacy settings for good?```"
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
