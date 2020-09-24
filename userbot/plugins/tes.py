2c067266ac6633846e29596a380cc265741f1853
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 11:53:05 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -8,6 +8,7 @@ from userbot import CMD_HELP, bot
 from subprocess import PIPE
 from subprocess import run as runapp
 from userbot.events import register, errors_handler
+from asyncio.exceptions import TimeoutError
 
 @register(outgoing=True, pattern=r"^\.decrypt(?: |$)(.*)")
 async def _(event):
@@ -39,8 +40,9 @@ async def _(event):
                     "`Can you kindly disable your forward privacy settings for good?`"
                 )
             else:
-                await event.edit(f"{response.message.message}")
-                await bot.send_read_acknowledge(conv.chat_id)
+                await bot.forward_messages(event.chat_id, respond.message)
+                await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
+                await event.delete()
     except TimeoutError:
         return await event.edit("`Error: is not responding!.`")
 
