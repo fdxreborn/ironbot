74eb622cfc5fe6b2c41e913f656b1f17709760d1
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 18:35:05 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -12,25 +12,25 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.edit("``````")
        return
     reply_message = await event.get_reply_message() 
     chat = "@AsalAja777_bot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.edit("``````")
        return
     await event.edit("```Making```")
     async with bot.conversation(chat) as conv:
           try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=Config.SENDER_ID))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @QuotLyBot and try again```")
+              await event.reply("``````")
               return
           if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+             await event.edit("``````")
           else: 
              await event.edit(f"{response.message.message}")
              await bot.forward_messages(event.chat_id, response.message)
