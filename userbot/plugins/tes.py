3cc63bf492286d0c70d72f254e6a944ddf2f3a20
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 11:58:22 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -10,40 +10,34 @@ from subprocess import run as runapp
 from userbot.events import register, errors_handler
 from asyncio.exceptions import TimeoutError
 
-@register(outgoing=True, pattern=r"^\.decrypt(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.hc (?:(now)|(.*) - (.*))")
 async def _(event):
     if event.fwd_from:
         return
-    if not event.reply_to_msg_id:
-        await event.edit("`Reply to any user message.`")
-        return
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        await event.edit("```reply to text message```")
-        return
     chat = "@hcdecryptor_bot"
-    await event.edit("`Processing`")
     try:
+        await event.edit("`Getting Your Music`")
         async with bot.conversation(chat) as conv:
+            await asyncio.sleep(2)
+            await event.edit("`Downloading...`")
             try:
                 response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=461843263)
+                    events.NewMessage(incoming=True, from_users=1025667425)
                 )
-                await bot.forward_messages(chat, reply_message)
-                response = await response
+                msg = await bot.send_message(chat)
+                respond = await response
+                res = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=1025667425)
+                )
+                r = await res
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
-                await event.reply("`Please unblock try again`")
+                await event.reply("`Unblock `@SpotifyMusicDownloaderBot` and retry`")
                 return
-            if response.text.startswith("Forward"):
-                await event.edit(
-                    "`Can you kindly disable your forward privacy settings for good?`"
-                )
-            else:
-                await bot.forward_messages(event.chat_id, respond.message)
-                await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
-                await event.delete()
+            await bot.forward_messages(event.chat_id, respond.message)
+        await event.client.delete_messages(conv.chat_id, [msg.id, r.id, respond.id])
+        await event.delete()
     except TimeoutError:
-        return await event.edit("`Error: is not responding!.`")
-
-
+        return await event.edit(
+            "`Error: `@SpotifyMusicDownloaderBot` is not responding or Song not found!.`"
+        )
