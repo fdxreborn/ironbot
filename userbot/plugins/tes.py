a0e7eb266f6ce67dceedb829b5b49273d9ff1df1
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 12:09:32 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -22,7 +22,7 @@ async def _(event):
                 response = conv.wait_event(
                     events.NewMessage(incoming=True, from_users=461843263)
                 )
-                await bot.forward_messages(chat, reply_message)
+                await bot.forward_messages(chat)
                 response = await response
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
