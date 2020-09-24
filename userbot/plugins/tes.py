ee73a84448b8ca697556f1dd0f84ad689e8f6486
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 19:52:33 +0700

    Update tes.py

--- userbot/plugins/tes.py
+++ userbot/plugins/tes.py
@@ -10,58 +10,38 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import bot, CMD_HELP
 from userbot.utils import admin_cmd
 
-#@register(outgoing=True, pattern="^.q(?: |$)(.*)")
-@borg.on(admin_cmd(pattern=r"hc(?: |$)(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.edit("``````")
-       return
-    link = event.pattern_match.group(1)
-    chat = "@AsalAja777_bot"
-    await event.edit("```Making```")
-    async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1178524273))
-              await bot.forward_messages(chat, link)
-              response = await response 
-              res = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=1178524273)
-              )
-              r = await res
-          except YouBlockedUserError: 
-              await event.reply("``````")
-              return
-          else: 
-             await bot.forward_messages(event.chat_id, respond.message)
-             await event.client.delete_messages(conv.chat_id,
-                                                [msg.id, r.id, respond.id])
-             await event.delete()
-
-#@register(outgoing=True, pattern="^.q(?: |$)(.*)")
-@borg.on(admin_cmd(pattern=r"sniff(?: |$)(.*)"))
+@borg.on(admin_cmd(pattern=("hc ?(.*)")))
 async def _(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
+    if not event.reply_to_msg_id:
+        await event.edit("```Reply to any user message.```")
+        return
+    reply_message = await event.get_reply_message()
     chat = "@AsalAja777_bot"
-    await event.edit("```Sedang mengambil musik yang anda inginkan```")
-    async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await event.edit("`Sedang Di Download, Stay disini aja`")
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              msg = await bot.send_message(chat, link)
-              respond = await response
-              res = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              r = await res
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await event.reply("```Tolong DiUnblock @SpotifyMusicDownloaderBot dan coba lagi```")
-              return
-          await bot.forward_messages(event.chat_id, respond.message)
-    await event.client.delete_messages(conv.chat_id,
-                                       [msg.id, r.id, respond.id])
-    await event.delete()
+    reply_message.sender
+    if reply_message.sender.bot:
+        await event.edit("```Reply to actual users message.```")
+        return
+    await event.edit("```Hemmm.....```")
+    async with borg.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=461843263)
+            )
+            await borg.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+            return
+        if response.text.startswith("Forward"):
+            await event.edit(
+                "The user have enabled privacy settings you cant get name history"
+            )
+        else:
+            await event.edit(f"`{response.message.message}`")
+            await event.edit(animu.chat_id,
+                            reply_to=event.reply_to_msg_id,
+                            silent=True if event.is_reply else False,
+                            hide_via=True)
+    
