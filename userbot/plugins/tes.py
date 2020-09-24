c8cc8cec8f7ee35bbf7ef4da60221c130448df45
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-24 11:45:51 +0700

    Add files via upload

---
+++ userbot/plugins/tes.py
@@ -0,0 +1,47 @@
+from telethon import events, functions
+import asyncio, io, types
+from uniborg.util import admin_cmd
+from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from uniborg.util import admin_cmd
+from userbot import CMD_HELP, bot
+from subprocess import PIPE
+from subprocess import run as runapp
+from userbot.events import register, errors_handler
+
+@register(outgoing=True, pattern=r"^\.decrypt(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.edit("`Reply to any user message.`")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.text:
+        await event.edit("```reply to text message```")
+        return
+    chat = "@hcdecryptor_bot"
+    await event.edit("`Processing`")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=461843263)
+                )
+                await bot.forward_messages(chat, reply_message)
+                response = await response
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock try again`")
+                return
+            if response.text.startswith("Forward"):
+                await event.edit(
+                    "`Can you kindly disable your forward privacy settings for good?`"
+                )
+            else:
+                await event.edit(f"{response.message.message}")
+                await bot.send_read_acknowledge(conv.chat_id)
+    except TimeoutError:
+        return await event.edit("`Error: is not responding!.`")
+
+
